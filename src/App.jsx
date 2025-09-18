40c2c9b2364456745395c9ac0efcaa55cd8c831f
ny-555 <nywork.fb1234@gmail.com>
2025-09-18 23:23:34 +0900

    feat:サイドバーの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log("ノートの追加");
+    const newNote = {
+      id: 1,
+      title: "new note",
+      content: "this is new note",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,24 @@
-import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = ({ onAddNote }) => {
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button onClick={onAddNote}>追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容です</p>
+          <small>修正日：</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
